MozillaSecurity/domfuzz

commit be94b87776bf2469a6a6331146ef41c382b4a398

Fix issue where createInstance(Components.interfaces.nsILocalFile) throws NS_ERROR_XPC_BAD_IID.

dom/extension/content/file.jsm
@@ -6,10 +6,7 @@
 
 function fileObject(path)
 {
-  var f = Components.classes["@mozilla.org/file/local;1"]
-                    .createInstance(Components.interfaces.nsILocalFile);
-  f.initWithPath(path);
-  return f;
+  return new Components.utils.import("resource://gre/modules/FileUtils.jsm").FileUtils.File(path);
 }
 
 

commit 645ead493311051065021b365ffab22267699dad

Fix break in extension introduced in be94b87776bf2469a6a6331146ef41c382b4a398.

dom/extension/content/file.jsm
@@ -6,7 +6,8 @@
 
 function fileObject(path)
 {
-  return new Components.utils.import("resource://gre/modules/FileUtils.jsm").FileUtils.File(path);
+  var FileUtils = Components.utils.import("resource://gre/modules/FileUtils.jsm").FileUtils;
+  return new FileUtils.File(path);
 }
 
 